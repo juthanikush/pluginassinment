647cbdec3e739f98fe5f76cfe74f2990fe902995
KUSH  JUTHNAI <81456337+juthanikush@users.noreply.github.com>
2023-06-02 23:00:56 +0530

    taxonomy hierarchical

--- wp-content/plugins/book/book.php
+++ wp-content/plugins/book/book.php
@@ -1,7 +1,7 @@
 <?php
 /**
 
-* Plugin Name: Book Custom post type
+* Plugin Name: Book
 
 * Plugin URI: managewp.com/blog
 
@@ -48,11 +48,38 @@ function create_posttype() {
         'hierarchical'=>false,
         'menu_position'=>null,
         'supports'=>array('title','editor','author','thumbnail','excerpt','comments')
+        
     );
 
 
-register_post_type( 'book',$args);
-
-
-}
+ register_post_type( 'book',$args);
+ }
 add_action( 'init', 'create_posttype' );
+
+function awesome_custome_taxonomies(){
+    //add new taxonomy hierarchical
+    $labels=array(
+        'name'=>'Types',
+        'singular_name'=>'Type',
+        'search_items'=>'Search Types',
+        'all_items'=>'All Types',
+        'parent_item'=>'Parent Type',
+        'parent_item_colon'=>'Parent Type',
+        'edit_item'=>'Edit Type',
+        'update_item'=>'Update Type',
+        'add_new_item'=>'Add New Type',
+        'new_item_name'=>'New Item Name',
+        'menu_name'=>'Type'
+    );
+
+    $args=array(
+        'hierarchical'=>true,
+        'labels'=>$labels,
+        'show_ui'=>true,
+        'show_admin_column'=>true,
+        'query_var'=>true,
+        'rewrite'=>array('slug'=>'type')
+    );
+    register_taxonomy('type',array('book'),$args);
+}
+add_action('init','awesome_custome_taxonomies');
